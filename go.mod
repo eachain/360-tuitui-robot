4293e45ce179907717c575e78e190a51bda47cc0
eachain <sg_siqing@163.com>
2024-11-06 10:53:31 +0800

    webhook redis cache required upgrade to v8

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module github.com/eachain/360-tuitui-robot
 
 go 1.21.13
 
-require github.com/go-redis/redis v6.15.9+incompatible
+require github.com/go-redis/redis/v8 v8.11.5
 
 require (
-	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/onsi/gomega v1.33.1 // indirect
 )
--- util/cache/redis.go
+++ util/cache/redis.go
@@ -1,9 +1,10 @@
 package cache
 
 import (
+	"context"
 	"time"
 
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v8"
 )
 
 // Redis为github.com/eachain/360-tuitui-robot/webhook.Cache的redis实现。
@@ -21,7 +22,7 @@ func NewRedis(cli *redis.Client, expire time.Duration) *Redis {
 // implement github.com/eachain/360-tuitui-robot/webhook.Cache.
 func (rds *Redis) Set(nonce string) bool {
 	key := "tuitui:robot:webhook:" + nonce
-	ok, err := rds.cli.SetNX(key, nonce, rds.expire).Result()
+	ok, err := rds.cli.SetNX(context.Background(), key, nonce, rds.expire).Result()
 	if err != nil {
 		// redis报错的情况下，允许业务继续执行。
 		// 考虑点：重复执行好过不执行。
