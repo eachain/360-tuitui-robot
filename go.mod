27cf9f95520a99172310cde56a2873d52a4d697f
eachain <sg_siqing@163.com>
2024-11-18 16:56:36 +0800

    去掉特定的redis驱动依赖

--- go.mod
+++ go.mod
@@ -1,11 +1,3 @@
 module github.com/eachain/360-tuitui-robot
 
 go 1.21.13
-
-require github.com/go-redis/redis/v8 v8.11.5
-
-require (
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/onsi/gomega v1.33.1 // indirect
-)
--- util/cache/redis.go
+++ util/cache/redis.go
@@ -2,27 +2,28 @@ package cache
 
 import (
 	"context"
-	"time"
-
-	"github.com/go-redis/redis/v8"
 )
+
+type Cache interface {
+	SetNX(ctx context.Context, key string, value string, expireSeconds int64) (bool, error)
+}
 
 // Redis为github.com/eachain/360-tuitui-robot/webhook.Cache的redis实现。
 // 用于分布式环境。
 type Redis struct {
-	cli    *redis.Client
-	expire time.Duration
+	cli    Cache
+	expire int64
 }
 
 // NewRedis复用业务所用*redis.Client。expire为nonce过期时间。
-func NewRedis(cli *redis.Client, expire time.Duration) *Redis {
-	return &Redis{cli: cli, expire: expire}
+func NewRedis(cli Cache, expireSeconds int64) *Redis {
+	return &Redis{cli: cli, expire: expireSeconds}
 }
 
 // implement github.com/eachain/360-tuitui-robot/webhook.Cache.
 func (rds *Redis) Set(nonce string) bool {
 	key := "tuitui:robot:webhook:" + nonce
-	ok, err := rds.cli.SetNX(context.Background(), key, nonce, rds.expire).Result()
+	ok, err := rds.cli.SetNX(context.Background(), key, nonce, rds.expire)
 	if err != nil {
 		// redis报错的情况下，允许业务继续执行。
 		// 考虑点：重复执行好过不执行。
